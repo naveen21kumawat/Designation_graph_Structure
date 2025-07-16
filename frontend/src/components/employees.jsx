737550108520baa9f8bd0cb392ff85cda39ba41b
Naveen Kumawat <naveendimond77@gmail.com>
2025-07-16 14:16:42 +0530

    Frontend  Code Updated

---
+++ frontend/src/components/employees.jsx
@@ -0,0 +1,30 @@
+export const flatEmployees = [
+  {
+    id: 1,
+    name: "Naveen Kumawat",
+    role: "CEO",
+    image: "https://avatars.githubusercontent.com/u/183393828?v=4&size=64",
+    parentId: null,
+  },
+  {
+    id: 2,
+    name: "Manish",
+    role: "CTO",
+    image: "",
+    parentId: 1,
+  },
+  {
+    id: 3,
+    name: "Mike Johnson",
+    role: "Developer",
+    image: "",
+    parentId: 2,
+  },
+  {
+    id: 3,
+    name: "Mike Johnson",
+    role: "Developer",
+    image: "",
+    parentId: 1,
+  },
+];
